eeacb2a156ef418cb7afb7e51553bba3611134b2
Glor Shin <sykkkkk412@naver.com>
2022-12-06 16:14:05 +0900

    style: reset, App.js layout

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import Home from "./components/Home";
 import PaymentManagement from "./components/payment-management/PaymentManagement";
 import PaymentRegistration from "./components/payment-registration/PaymentRegistration";
 import RegistrationComplete from "./components/registration-complete/RegistrationComplete";
+import './App.scss';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path={'/'} element={<Home />}/>
-        <Route path={'/management'} element={<PaymentManagement />} />
-        <Route path={'/registration'} element={<PaymentRegistration />} />
-        <Route path={'/complete'} element={<RegistrationComplete />} />
-      </Routes>
-    </BrowserRouter>
+    <div className={'App'}>
+      <BrowserRouter>
+        <Routes>
+          <Route path={'/'} element={<Home />}/>
+          <Route path={'/management'} element={<PaymentManagement />} />
+          <Route path={'/registration'} element={<PaymentRegistration />} />
+          <Route path={'/complete'} element={<RegistrationComplete />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
