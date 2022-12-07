8f124c5b3d4acc07fc7c805d14089532b48e5730
Glor Shin <sykkkkk412@naver.com>
2022-12-07 13:58:41 +0900

    style: Add CompleteText

--- src/components/registration-complete/CompleteText.js
+++ src/components/registration-complete/CompleteText.js
@@ -1,1 +1,19 @@
-registration-complete
+import React from 'react';
+import PropTypes from 'prop-types';
+import './CompleteText.scss';
+
+CompleteText.propTypes = {
+  title: PropTypes.string.isRequired,
+  subTitle: PropTypes.element
+};
+
+function CompleteText({ title, subTitle }) {
+  return (
+    <div className={'complete-text-wrapper'}>
+      <div className={'title'}>{title}</div>
+      <div className={'sub-title'}> {subTitle}</div>
+    </div>
+  );
+}
+
+export default CompleteText;
