7f4707e43c29c2cb67eb08f6ba27294f86ed1546
Glor Shin <sykkkkk412@naver.com>
2022-12-07 13:47:48 +0900

    style: Add PamentRegistration

--- src/components/payment-registration/PaymentRegistration.js
+++ src/components/payment-registration/PaymentRegistration.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import './PaymentRegistration.scss';
+import TitleBox from "../TitleBox";
+import Button from "../Button";
+import CardRegister from "./CardRegister";
+import Keypad from "./Keypad";
 
-function PaymentRegistration(props) {
+function PaymentRegistration() {
   return (
-    <div></div>
+    <div className={'payment-registration-wrapper'}>
+      <TitleBox titleText={'결제 수단 등록'} />
+      <CardRegister />
+      <Keypad />
+      <Button buttonText={'등록'} />
+    </div>
   );
 }
 
