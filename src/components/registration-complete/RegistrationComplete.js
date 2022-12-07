68f0b976364bdfc3c718cfbc21b74ddaf0b75c15
Glor Shin <sykkkkk412@naver.com>
2022-12-07 14:30:05 +0900

    style: Add RegistrationComplete

--- src/components/registration-complete/RegistrationComplete.js
+++ src/components/registration-complete/RegistrationComplete.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import './RegistrationComplete.scss';
+import TitleBox from "../TitleBox";
+import Button from "../Button";
+import CompleteText from "./CompleteText";
+import CompleteCard from "./CompleteCard";
 
-function RegistrationComplete(props) {
+export default function RegistrationComplete() {
   return (
-    <div></div>
+    <div className={'registration-complete-wrapper'}>
+      <TitleBox titleText={'결제 수단 등록 완료'} />
+      <CompleteCard />
+      <CompleteText title={'등록완료!'} subTitle={<>와우! 결제를 위한 준비가 모두 끝났습니다.<br/>쉽고 빠른 스마일페이를 바로 사용해 보세요.</>}/>
+      <Button buttonText={'확인'} />
+    </div>
   );
 }
-
-export default RegistrationComplete;
