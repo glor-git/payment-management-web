ac7c7515a73a9c13ab5924bdea7d1a6d4c6bbb70
Glor Shin <sykkkkk412@naver.com>
2022-12-08 17:20:21 +0900

    feat: 키패드 기능개발

--- src/components/payment-registration/Keypad.js
+++ src/components/payment-registration/Keypad.js
@@ -1,9 +1,20 @@
 import React, { useEffect, useState } from 'react';
+import PropTypes from "prop-types";
 import styles from './Keypad.module.scss';
 import { shuffle } from "../../util/array";
 
-export default function Keypad() {
+Keypad.propTypes = {
+  maxLength: PropTypes.number,
+  setNumber: PropTypes.func.isRequired
+};
+
+export default function Keypad({ maxLength, setNumber }) {
   const [padArr, setPadArr] = useState([]);
+
+  function handleSetNumber(number) {
+    if (number === 'x') setNumber(prev => prev.slice(0, -1));
+    else setNumber(prev => (prev + number).length <= maxLength ? prev + number : prev)
+  }
 
   useEffect(() => {
     setPadArr(shuffle([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, null]));
@@ -13,10 +24,12 @@ export default function Keypad() {
     <div className={styles.keypadWrapper}>
       {padArr.map(number => {
         return (
-          <div className={styles.key}>{number}</div>
+          <button className={styles.key} key={number} onClick={() => handleSetNumber(number)}>
+            {number === null ? <>&nbsp;</> : number}
+          </button>
         )
       })}
-      <div className={styles.key}>x</div>
+      <button className={styles.key} onClick={() => handleSetNumber('x')}>x</button>
     </div>
   );
 }
