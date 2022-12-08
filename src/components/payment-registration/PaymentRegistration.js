1794a4df23fef2ddeb4694bd1dbd9a836976b4f5
Glor Shin <sykkkkk412@naver.com>
2022-12-08 17:21:16 +0900

    feat: 데이터 저장 준비

--- src/components/payment-registration/PaymentRegistration.js
+++ src/components/payment-registration/PaymentRegistration.js
@@ -3,15 +3,31 @@ import styles from './PaymentRegistration.module.scss';
 import TitleBox from "../TitleBox";
 import Button from "../Button";
 import CardRegister from "./CardRegister";
-import Keypad from "./Keypad";
 
 function PaymentRegistration() {
+
+  function getUserCardData() {
+    const cardNumbers = document.getElementsByName('cardNumber');
+    const cardDates = document.getElementsByName('cardDate');
+    const cardPasswords = document.getElementsByName('cardPassword');
+    let cardNumber = '', cardDate = '', cardPassword = '';
+    cardNumbers.forEach(number => cardNumber = cardNumber + number.value)
+    cardDates.forEach(number => cardDate = cardDate + number.value)
+    cardPasswords.forEach(number => cardPassword = cardPassword + number.value)
+
+    return { cardNumber, cardDate, cardPassword }
+  }
+
+  function setData() {
+    const { cardNumber, cardDate, cardPassword } = getUserCardData();
+  }
+
+
   return (
     <div className={styles.paymentRegistrationWrapper}>
       <TitleBox titleText={'결제 수단 등록'} />
       <CardRegister />
-      <Keypad />
-      <Button buttonText={'등록'} />
+      <Button buttonText={'등록'} clickEvent={setData} />
     </div>
   );
 }
