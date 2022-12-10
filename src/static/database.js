cd554b0a5b5a7f064525f548b5b5d5c171ff6ef1
Glor Shin <sykkkkk412@naver.com>
2022-12-10 14:35:27 +0900

    feat: 카드 데이터베이스 데이터 추가

---
+++ src/static/database.js
@@ -0,0 +1,18 @@
+export const cardListData = [
+  {
+    cardId: 1,
+    cardImage: '',
+    cardName: '신한카드',
+    cardNumber: '1111111111111111',
+    cardDate: '1111',
+    cardPassword: '11'
+  },
+  {
+    cardId: 2,
+    cardImage: '',
+    cardName: '우리카드',
+    cardNumber: '2222222222222222',
+    cardDate: '2222',
+    cardPassword: '22'
+  }
+]
