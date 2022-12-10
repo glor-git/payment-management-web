2c246177794197161e801023f56305d3a4638a15
Glor Shin <sykkkkk412@naver.com>
2022-12-10 14:35:44 +0900

    feat: 카드 등록 기능 개발

--- src/components/payment-registration/PaymentRegistration.js
+++ src/components/payment-registration/PaymentRegistration.js
@@ -1,10 +1,13 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import styles from './PaymentRegistration.module.scss';
 import TitleBox from "../TitleBox";
 import Button from "../Button";
 import CardRegister from "./CardRegister";
+import { useNavigate } from 'react-router-dom';
+import { cardListData } from "../../static/database";
 
 export default function PaymentRegistration() {
+  const navigate = useNavigate();
 
   function getUserCardData() {
     const cardNumbers = document.getElementsByName('cardNumber');
@@ -18,22 +21,40 @@ export default function PaymentRegistration() {
     return { cardNumber, cardDate, cardPassword }
   }
 
-  function setData() {
+  function findCard() {
     const { cardNumber, cardDate, cardPassword } = getUserCardData();
-    const cardData = {
-      cardNumber,
-      cardDate,
-      cardPassword
-    }
-    if (localStorage.getItem('cardData')) localStorage.setItem('cardData', JSON.stringify([...JSON.parse(localStorage.getItem('cardData')), cardData]));
-    else localStorage.setItem('cardData', JSON.stringify([cardData]));
+    let isCard = false;
+    let card = {}
+
+    cardListData.forEach(cardData => {
+      if (cardData.cardNumber === cardNumber && cardData.cardDate === cardDate && cardData.cardPassword === cardPassword) {
+        isCard = true;
+        card = {
+          cardId: cardData.cardId,
+          cardImage: cardData.cardImage,
+          cardName: cardData.cardName,
+          cardNumber: cardData.cardNumber,
+          cardDate: cardData.cardDate,
+          cardPassword: cardData.cardPassword
+        }
+        return true
+      }
+    })
+
+    return { isCard, card }
   }
 
-  useEffect(() => {
-    console.log(JSON.parse(localStorage.getItem('cardData')));
-  }, [])
-
-
+  function setData() {
+    if (findCard().isCard) {
+      if (localStorage.getItem('cardData')) localStorage.setItem('cardData', JSON.stringify([...JSON.parse(localStorage.getItem('cardData')), findCard().card]));
+      else localStorage.setItem('cardData', JSON.stringify([findCard().card]));
+      alert('등록되었습니다.');
+      navigate('/complete');
+    } else {
+      alert('카드 정보를 찾을 수 없습니다.');
+    }
+  }
+  // console.log(JSON.parse(localStorage.getItem('cardData')));
   return (
     <div className={styles.paymentRegistrationWrapper}>
       <TitleBox titleText={'결제 수단 등록'} />
